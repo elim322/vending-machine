c2f005c2ccf1fbee63ef88ac6b40bc3ff2db4829
elim322 <einer.lim322@gmail.com>
2018-11-18 18:16:20 -0800

    add functions for dispense refill and stock

--- __tests__/vending-machine.spec.js
+++ __tests__/vending-machine.spec.js
@@ -8,9 +8,9 @@ describe("VendingMachine", () => {
       vendingMachine = new VendingMachine("../data.json");
     });
     describe("currentStock", () => {
-      it("should return current number of bottles", () => {
+      it("should return name, quantity, price, and code of products", () => {
         expect(vendingMachine.queryStock()).toEqual(
-          "coke: 5, pepsi: 7, sprite: 9, water: 4, orange: 3, apple: 8"
+          "coke: 5 135 A1, pepsi: 7 130 A2, sprite: 9 130 B1, water: 4 120 B2, orange: 3 90 C1, apple: 8 90 C2"
         );
       });
     });
@@ -19,9 +19,22 @@ describe("VendingMachine", () => {
     beforeEach(() => {
       vendingMachine = new VendingMachine("../data.json");
     });
-    describe("dispense", () => {
-      it("should return one one less coke", () => {
-        expect(vendingMachine.queryEmpty()).toEqual("coke: 4");
+    describe("should dispense one coke with the correct change", () => {
+      it("should return one less coke quantity", () => {
+        expect(vendingMachine.queryDispense("A1")).toEqual(4);
+      });
+    });
+  });
+  describe("queryRefill", () => {
+    beforeEach(() => {
+      vendingMachine = new VendingMachine("../data.json");
+      vendingMachine.queryDispense("A2");
+      console.log(vendingMachine.queryStock());
+    });
+    describe("should refill stock", () => {
+      it("should refill contents of vending machine", () => {
+        expect(vendingMachine.queryRefill()).toEqual("items refilled");
+        console.log(vendingMachine.queryStock());
       });
     });
   });
